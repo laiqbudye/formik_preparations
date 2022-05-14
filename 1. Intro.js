9c11d275bdd69027cab35849ebd5a972151fe732
Laiq Budye <40020518+laiqbudye@users.noreply.github.com>
2022-05-14 16:03:06 +0530

    Create 1. Intro.js

---
+++ 1. Intro.js
@@ -0,0 +1,13 @@
+Formik  
+    is a small library that helps to deal with forms in react.
+    
+why Formik
+  - managing form data
+  - form submission
+  - form validation & displaying error msgs
+  
+  
+ we can do all these things with plain react as well but Formik allows us to do that thing in a scalable, performant & easier way.
+ 
+ 
+to install:- npm i formik
